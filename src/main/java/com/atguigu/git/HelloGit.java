a01f4906134c19a51680247150fd59b5225a832c
jkwe9746 <jkwe7777@qq.com>
2020-07-12 19:12:51 +0800

    eat food

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -2,12 +2,8 @@ package com.atguigu.git;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("赂眉脨脗!");
-		System.out.println("Branch1");
-		System.out.println("Branch2");
-
-		System.out.println("struggle");
-		System.out.println("第二次更新了");
-		
+		System.out.println("first:I love jeag");
+		System.out.println("second:I love jeag");
+		System.out.println("three:I love jeag");		
 	}
 }
