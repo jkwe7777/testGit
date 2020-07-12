6ef0e0ba8d3ba219dd69fa6247d769aa529ee3be
jkwe9746 <jkwe7777@qq.com>
2020-07-12 16:58:53 +0800

    I am merge

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -3,6 +3,8 @@ package com.atguigu.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("¸üÐÂ!");
-		System.out.println("Branch");
+		System.out.println("Branch1");
+		System.out.println("Branch2");
+		System.out.println("struggle");
 	}
 }
