aef4ad5b0304d770f2db07cfc9cedb2c99c8be0e
jkwe9746 <jkwe7777@qq.com>
2020-07-13 16:19:37 +0800

    继续carry on

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -3,8 +3,14 @@ package com.atguigu.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("first:I love jeag");
-		System.out.println("second:I love jeag");
+		System.out.println("second:I love jeag");  
 		System.out.println("three:I love jeag");		
 		System.out.println("four:I love jeag");		
+		System.out.println("five:I love jeag");		
+		System.out.println("six:I love jeag");		
+		System.out.println("seven:I love jeag");		
+		System.out.println("eight:I love jeag");		
+		System.out.println("nine:I love jeag");		
+		System.out.println("zero:I love jeag");		
 	}
 }
